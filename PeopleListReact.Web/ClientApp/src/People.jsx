1c6817851eba5f1d40b64b3ad4e7f726058a50aa
Rochel Chaifetz <rochelchaifetz@gmail.com>
2023-05-23 23:40:16 -0400

    Add project files.

---
+++ PeopleListReact.Web/ClientApp/src/People.jsx
@@ -0,0 +1,76 @@
+﻿import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PersonForm from './PersonForm';
+import PersonRow from './PersonRow';
+
+class People extends React.Component {
+
+    state = {
+        firstName: '',
+        lastName: '',
+        age: '',
+        people: []
+    }
+
+    onFirstNameChange = (e) => {
+        this.setState({ firstName: e.target.value });
+    }
+
+    onLastNameChange = (e) => {
+        this.setState({ lastName: e.target.value });
+    }
+
+    onAgeChange = (e) => {
+        this.setState({ age: e.target.value });
+    }
+
+    onAddClick = () => {
+        const copy = [...this.state.people];
+        const person = {
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            age: this.state.age
+        }
+        copy.push(person);
+        this.setState({ people: copy, firstName: '', lastName: '', age: '' });
+    }
+
+    onClearClick = () => {
+        this.setState({ people: [], firstName: '', lastName: '', age: '' });
+    }
+
+    render() {
+        return (<>
+            <div className='container mt-5'>
+                <PersonForm onClearClick={this.onClearClick}
+                    onAddClick={this.onAddClick}
+                    onFirstNameChange={this.onFirstNameChange}
+                    onLastNameChange={this.onLastNameChange}
+                    onAgeChange={this.onAgeChange}
+                    firstName={this.state.firstName}
+                    lastName={this.state.lastName}
+                    age={this.state.age} />
+
+                {this.state.people.length === 0 ?
+                    <h1>No people added yet! Add some people!</h1> :
+                    <table className='table table-hover table-striped table-bordered'>
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Age</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <PersonRow people={this.state.people} />
+                        </tbody>
+                    </table>
+                }
+            </div>
+        </>);
+    };
+
+};
+
+export default People;
+
---
+++ PeopleListReact.Web/ClientApp/src/PersonForm.jsx
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class PersonForm extends React.Component {
+    render() {
+        return (
+            <>
+                <div className='container mt-5'>
+                    <div className='row'>
+                        <div className='col-md-3'>
+                            <input value={this.props.firstName} onChange={this.props.onFirstNameChange} type='text' placeholder='First Name' className='form-control' />
+                        </div>
+                        <div className='col-md-3'>
+                            <input value={this.props.lastName} onChange={this.props.onLastNameChange} type='text' placeholder='Last Name' className='form-control' />
+                        </div>
+                        <div className='col-md-3'>
+                            <input value={this.props.age} onChange={this.props.onAgeChange} type='text' placeholder='Age' className='form-control' />
+                        </div>
+                        <div className='col-md-1'>
+                            <button onClick={this.props.onAddClick} className='btn btn-primary w-100'>Add</button>
+
+                        </div>
+                        <div className='col-md-1'>
+                            <button onClick={this.props.onClearClick} className='btn btn-warning w-100'>Clear</button>
+                        </div>
+
+                    </div>
+                </div>
+            </>
+
+        );
+    }
+}
+
+export default PersonForm;
